185bb7809e724fc7ab0e47d4b1c3e9a6606d474d
yue002 <114208281+yue002@users.noreply.github.com>
2024-12-25 19:51:43 +0700

    Create Cow.java

---
+++ Cow.java
@@ -0,0 +1,9 @@
+public class Cow extends Animal{
+    @Override
+    void sound() {
+        System.out.println("moo moo");
+    }
+    void milk(){
+        System.out.println("... now loading...");
+    }
+}
