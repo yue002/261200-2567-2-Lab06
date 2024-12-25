476d7ffd48dc73437c3643f6998b9a62a6ab3214
yue002 <114208281+yue002@users.noreply.github.com>
2024-12-25 19:59:49 +0700

    Create Lec06.java

---
+++ Lec06.java
@@ -0,0 +1,26 @@
+public class main {
+    public static void main(String[] args) {
+        Cow cow = new Cow();
+        cow.sound();
+
+        Duck donald = new Duck("โดนัลด์");
+        donald.sound();
+
+        Pig burin = new Pig();
+        burin.sound();
+
+        Owl hedwig = new Owl();
+        hedwig.sound();
+
+        donald.clean(cow);
+
+        Duck daisy = new Duck("เดซี่");
+        daisy.clean(donald);
+
+        Duck fourseason = new pekingDuck("4ss");
+        fourseason.clean(daisy);
+        fourseason.fly();
+
+        donald.clean(new Cow());
+    }
+}
